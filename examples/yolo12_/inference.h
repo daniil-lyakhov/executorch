74decf61fa453e8a83d685c32724f53444efefdc
dlyakhov <daniil.lyakhov@intel.com>
2025-04-03 14:36:58 +0200

    Refactoring

--- examples/yolo12_/inference.h
+++ examples/yolo12_/inference.h
@@ -1,83 +1,167 @@
 #ifndef INFERENCE_H
 #define INFERENCE_H
 
-// Cpp native
-#include <fstream>
-#include <vector>
-#include <string>
+#include <getopt.h>
+#include <iostream>
 #include <random>
+#include <vector>
 
-// OpenCV / DNN / Inference
-#include <opencv2/imgproc.hpp>
-#include <opencv2/opencv.hpp>
-#include <opencv2/dnn.hpp>
-
-
-#include <executorch/extension/data_loader/file_data_loader.h>
-#include <executorch/extension/evalue_util/print_evalue.h>
-#include <executorch/extension/runner_util/inputs.h>
-#include <executorch/runtime/core/event_tracer.h>
-#include <executorch/runtime/executor/method.h>
-#include <executorch/runtime/executor/program.h>
-#include <executorch/runtime/platform/log.h>
-#include <executorch/runtime/platform/platform.h>
+#include <executorch/extension/module/module.h>
+#include <executorch/extension/tensor/tensor.h>
+#include <executorch/runtime/core/error.h>
+#include <executorch/runtime/core/evalue.h>
+#include <executorch/runtime/core/exec_aten/exec_aten.h>
+#include <executorch/runtime/core/result.h>
 #include <executorch/runtime/platform/runtime.h>
+#include <opencv2/opencv.hpp>
 
-
-using executorch::extension::FileDataLoader;
-using executorch::extension::BufferCleanup;
+using executorch::aten::ScalarType;
+using executorch::aten::Tensor;
+using executorch::extension::from_blob;
+using executorch::extension::Module;
 using executorch::runtime::Error;
 using executorch::runtime::EValue;
-using executorch::runtime::EventTracer;
-using executorch::runtime::HierarchicalAllocator;
-using executorch::runtime::MemoryAllocator;
-using executorch::runtime::MemoryManager;
-using executorch::runtime::Method;
-using executorch::runtime::MethodMeta;
-using executorch::runtime::Program;
 using executorch::runtime::Result;
-using executorch::runtime::Span;
-using executorch::runtime::TensorInfo;
 
-struct Detection
-{
-    int class_id{0};
-    std::string className{};
-    float confidence{0.0};
-    cv::Scalar color{};
-    cv::Rect box{};
+struct Detection {
+  int class_id{0};
+  std::string className{};
+  float confidence{0.0};
+  cv::Scalar color{};
+  cv::Rect box{};
 };
 
-class YoloInference
-{
-public:
-    YoloInference(const std::string &exported_model_path, const cv::Size &input_shape = {640, 640});
-    std::vector<Detection> runInference(const cv::Mat &input);
-
-private:
-    cv::Mat formatToSquare(const cv::Mat &source, int *pad_x, int *pad_y, float *scale);
-
-    std::string modelPath{};
-    std::string classesPath{};
-
-    std::vector<std::string> classes{"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"};
-
-    cv::Size2f modelShape{};
-
-    Program Program_;
-    Method Method_;
-    std::string MethodName;
-
-    static uint8_t MethodAllocatorPool[4 * 1024U * 1024U]; // 4 MB
-    static uint8_t TMPAllocatorPool[1024U * 1024U];
-    std::vector<std::unique_ptr<uint8_t[]>> PlannedBuffers; // Owns the memory
-    std::vector<Span<uint8_t>> PlannedSpans; // Passed to the allocator
-
-    float modelConfidenceThreshold {0.25};
-    float modelScoreThreshold      {0.45};
-    float modelNMSThreshold        {0.50};
-
-    bool letterBoxForSquare = true;
+struct DetectionConfig {
+  std::vector<std::string> classes;
+  float modelScoreThreshold;
+  float modelNMSThreshold;
 };
 
+cv::Mat scale_with_padding(
+    cv::Mat& source,
+    int* pad_x,
+    int* pad_y,
+    float* scale,
+    cv::Size img_dims) {
+  int col = source.cols;
+  int row = source.rows;
+  int m_inputWidth = img_dims.width;
+  int m_inputHeight = img_dims.height;
+  if (col == m_inputWidth and row == m_inputHeight) {
+    return source;
+  }
+
+  *scale = std::min(m_inputWidth / (float)col, m_inputHeight / (float)row);
+  int resized_w = col * *scale;
+  int resized_h = row * *scale;
+  *pad_x = (m_inputWidth - resized_w) / 2;
+  *pad_y = (m_inputHeight - resized_h) / 2;
+
+  cv::Mat resized;
+  cv::resize(source, resized, cv::Size(resized_w, resized_h));
+  cv::Mat result = cv::Mat::zeros(m_inputHeight, m_inputWidth, source.type());
+  resized.copyTo(result(cv::Rect(*pad_x, *pad_y, resized_w, resized_h)));
+  resized.release();
+  return result;
+}
+
+std::vector<Detection> infer_yolo_once(
+    Module& module,
+    cv::Mat input,
+    const DetectionConfig yolo_config) {
+  const auto model_input_shape =
+      module.method_meta("forward")->input_tensor_meta(0)->sizes();
+  cv::Size img_dims = {model_input_shape[2], model_input_shape[3]};
+
+  int pad_x, pad_y;
+  float scale;
+  input = scale_with_padding(input, &pad_x, &pad_y, &scale, img_dims);
+
+  cv::Mat blob;
+  cv::dnn::blobFromImage(
+      input, blob, 1.0 / 255.0, img_dims, cv::Scalar(), true, false);
+  const auto t_input = from_blob(
+      (void*)blob.data,
+      std::vector<int>(blob.size.p, blob.size.p + blob.dims),
+      ScalarType::Float);
+  const auto result = module.forward(t_input);
+
+  ET_CHECK_MSG(result.ok(), "Could not infer the model with an error");
+
+  const auto t = result->at(0).toTensor(); // Using only the 0 output
+  cv::Mat mat_output(t.dim(), t.sizes().data(), CV_32FC1, t.data_ptr());
+
+  // yolov8 has an output of shape (batchSize, 84,  8400) (Num classes +
+  // box[x,y,w,h])
+  int rows = mat_output.size[2];
+  int dimensions = mat_output.size[1];
+
+  mat_output = mat_output.reshape(1, dimensions);
+  cv::transpose(mat_output, mat_output);
+
+  float* data = (float*)mat_output.data;
+
+  std::vector<int> class_ids;
+  std::vector<float> confidences;
+  std::vector<cv::Rect> boxes;
+
+  for (int i = 0; i < rows; ++i) {
+    float* classes_scores = data + 4;
+
+    cv::Mat scores(1, yolo_config.classes.size(), CV_32FC1, classes_scores);
+    cv::Point class_id;
+    double max_class_score;
+
+    cv::minMaxLoc(scores, 0, &max_class_score, 0, &class_id);
+
+    if (max_class_score > yolo_config.modelScoreThreshold) {
+      confidences.push_back(max_class_score);
+      class_ids.push_back(class_id.x);
+
+      float x = data[0];
+      float y = data[1];
+      float w = data[2];
+      float h = data[3];
+
+      int left = int((x - 0.5 * w - pad_x) / scale);
+      int top = int((y - 0.5 * h - pad_y) / scale);
+
+      int width = int(w / scale);
+      int height = int(h / scale);
+
+      boxes.push_back(cv::Rect(left, top, width, height));
+    }
+
+    data += dimensions;
+  }
+
+  std::vector<int> nms_result;
+  cv::dnn::NMSBoxes(
+      boxes,
+      confidences,
+      yolo_config.modelScoreThreshold,
+      yolo_config.modelNMSThreshold,
+      nms_result);
+
+  std::vector<Detection> detections{};
+  for (unsigned long i = 0; i < nms_result.size(); ++i) {
+    int idx = nms_result[i];
+
+    Detection result;
+    result.class_id = class_ids[idx];
+    result.confidence = confidences[idx];
+
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<int> dis(100, 255);
+    result.color = cv::Scalar(dis(gen), dis(gen), dis(gen));
+
+    result.className = yolo_config.classes[result.class_id];
+    result.box = boxes[idx];
+
+    detections.push_back(result);
+  }
+
+  return detections;
+}
 #endif // INFERENCE_H
--- examples/yolo12_/main.cpp
+++ examples/yolo12_/main.cpp
@@ -1,280 +1,106 @@
-#include <iostream>
-#include <vector>
-#include <getopt.h>
-#include <random>
-#include <csignal>
+#include "inference.h"
 
-#include <opencv2/opencv.hpp>
-#include <executorch/runtime/core/exec_aten/exec_aten.h>
-#include <executorch/runtime/platform/runtime.h>
-#include <executorch/runtime/core/evalue.h>
-#include <executorch/runtime/core/error.h>
-#include <executorch/extension/module/module.h>
+void draw_detection(cv::Mat& frame, const Detection detection);
 
+DetectionConfig DEFAULT_YOLO_CONFIG = {
+    {"person",        "bicycle",      "car",
+     "motorcycle",    "airplane",     "bus",
+     "train",         "truck",        "boat",
+     "traffic light", "fire hydrant", "stop sign",
+     "parking meter", "bench",        "bird",
+     "cat",           "dog",          "horse",
+     "sheep",         "cow",          "elephant",
+     "bear",          "zebra",        "giraffe",
+     "backpack",      "umbrella",     "handbag",
+     "tie",           "suitcase",     "frisbee",
+     "skis",          "snowboard",    "sports ball",
+     "kite",          "baseball bat", "baseball glove",
+     "skateboard",    "surfboard",    "tennis racket",
+     "bottle",        "wine glass",   "cup",
+     "fork",          "knife",        "spoon",
+     "bowl",          "banana",       "apple",
+     "sandwich",      "orange",       "broccoli",
+     "carrot",        "hot dog",      "pizza",
+     "donut",         "cake",         "chair",
+     "couch",         "potted plant", "bed",
+     "dining table",  "toilet",       "tv",
+     "laptop",        "mouse",        "remote",
+     "keyboard",      "cell phone",   "microwave",
+     "oven",          "toaster",      "sink",
+     "refrigerator",  "book",         "clock",
+     "vase",          "scissors",     "teddy bear",
+     "hair drier",    "toothbrush"},
+    0.45,
+    0.50};
 
-using namespace std;
-using namespace cv;
-using executorch::extension::Module;
-using executorch::runtime::EValue;
+int main(int argc, char** argv) {
+  std::string projectBasePath =
+      "/home/dlyakhov/Projects/ultralytics"; // Set your ultralytics base path
 
-#include <executorch/extension/tensor/tensor.h>
-#include <executorch/runtime/core/evalue.h>
-#include <executorch/runtime/core/exec_aten/exec_aten.h>
-#include <executorch/runtime/core/result.h>
+  executorch::runtime::runtime_init();
+  Module yolo_module(projectBasePath + "/yolov8s.pte");
 
-using executorch::aten::ScalarType;
-using executorch::aten::Tensor;
-using executorch::extension::from_blob;
-using executorch::runtime::Result;
-using executorch::runtime::Error;
+  auto error = yolo_module.load();
+  // TODO: enable
+  // ET_CHECK_MSG(
+  //    error == Error::ok ,
+  //    "Failed to parse model file %s, error: 0x%",
+  //    projectBasePath.c_str(), error);
 
-cv::Mat format_to_square(cv::Mat &source, int *pad_x, int *pad_y, float *scale, cv::Size model_shape)
-{
-    int col = source.cols;
-    int row = source.rows;
-    int m_inputWidth = model_shape.width;
-    int m_inputHeight = model_shape.height;
+  error = yolo_module.load_forward();
+  // ET_CHECK_MSG(
+  //     error == Error::ok ,
+  //     "Failed to get method_meta for forward, error: 0x%", error);
 
-    *scale = std::min(m_inputWidth / (float)col, m_inputHeight / (float)row);
-    int resized_w = col * *scale;
-    int resized_h = row * *scale;
-    *pad_x = (m_inputWidth - resized_w) / 2;
-    *pad_y = (m_inputHeight - resized_h) / 2;
+  std::vector<std::string> imageNames;
+  imageNames.push_back(projectBasePath + "/ultralytics/assets/bus.jpg");
+  imageNames.push_back(projectBasePath + "/ultralytics/assets/zidane.jpg");
 
-    cv::Mat resized;
-    cv::resize(source, resized, cv::Size(resized_w, resized_h));
-    cv::Mat result = cv::Mat::zeros(m_inputHeight, m_inputWidth, source.type());
-    resized.copyTo(result(cv::Rect(*pad_x, *pad_y, resized_w, resized_h)));
-    resized.release();
-    return result;
+  for (int i = 0; i < imageNames.size(); ++i) {
+    cv::Mat frame = cv::imread(imageNames[i]);
+
+    std::vector<Detection> output =
+        infer_yolo_once(yolo_module, frame, DEFAULT_YOLO_CONFIG);
+
+    std::cout << "Number of detections:" << output.size() << std::endl;
+
+    for (auto& detection : output) {
+      draw_detection(frame, detection);
+    }
+
+    // This is only for preview purposes
+    float scale = 0.8;
+    cv::resize(frame, frame, cv::Size(frame.cols * scale, frame.rows * scale));
+    // std::raise(SIGINT);
+    cv::imwrite("out_4125_" + std::to_string(i) + ".jpg", frame);
+    // cv::imshow("Inference", frame);
+    // cv::waitKey(-1);
+  }
 }
 
-struct Detection
-{
-    int class_id{0};
-    std::string className{};
-    float confidence{0.0};
-    cv::Scalar color{};
-    cv::Rect box{};
-};
+void draw_detection(cv::Mat& frame, const Detection detection) {
+  cv::Rect box = detection.box;
+  cv::Scalar color = detection.color;
 
-std::vector<Detection> infer_yolo_once(Module& module, cv::Mat input)
-{
-    // TODO: figure out how to work with a shape
-    cv::Size model_shape = {640, 640};
-    // TODO: Keep it somwhere else
-    std::vector<std::string> classes{"person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"};
-    float modelConfidenceThreshold {0.25};
-    float modelScoreThreshold      {0.45};
-    float modelNMSThreshold        {0.50};
+  // Detection box
+  cv::rectangle(frame, box, color, 2);
 
+  // Detection box text
+  std::string classString = detection.className + ' ' +
+      std::to_string(detection.confidence).substr(0, 4);
+  cv::Size textSize =
+      cv::getTextSize(classString, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
+  cv::Rect textBox(
+      box.x, box.y - 40, textSize.width + 10, textSize.height + 20);
 
-    int pad_x, pad_y;
-    float scale;
-    input = format_to_square(input, &pad_x, &pad_y, &scale, model_shape);
-
-    cv::Mat blob;
-    cv::dnn::blobFromImage(input, blob, 1.0/255.0, model_shape, cv::Scalar(), true, false);
-    const auto t_input = from_blob(
-            (void*)blob.data,                     // Raw data pointer
-            std::vector<int>(blob.size.p, blob.size.p + blob.dims),
-            ScalarType::Float
-        );
-    const auto e_outputs = module.execute("forward", {EValue(t_input)});
-    const auto t = e_outputs.get()[0].toTensor(); // Using only the 0 output
-
-    const cv::Mat mat_output(t.dim(), t.sizes().data(), CV_32FC1, t.data_ptr());
-    auto outputs = std::vector<cv::Mat>();
-    outputs.push_back(mat_output);
-
-    int rows = outputs[0].size[1];
-    int dimensions = outputs[0].size[2];
-
-    bool yolov8 = false;
-    // yolov5 has an output of shape (batchSize, 25200, 85) (Num classes + box[x,y,w,h] + confidence[c])
-    // yolov8 has an output of shape (batchSize, 84,  8400) (Num classes + box[x,y,w,h])
-    if (dimensions > rows) // Check if the shape[2] is more than shape[1] (yolov8)
-    {
-        yolov8 = true;
-        rows = outputs[0].size[2];
-        dimensions = outputs[0].size[1];
-
-        outputs[0] = outputs[0].reshape(1, dimensions);
-        cv::transpose(outputs[0], outputs[0]);
-    }
-    float *data = (float *)outputs[0].data;
-
-    std::vector<int> class_ids;
-    std::vector<float> confidences;
-    std::vector<cv::Rect> boxes;
-
-    for (int i = 0; i < rows; ++i)
-    {
-        if (yolov8)
-        {
-            float *classes_scores = data+4;
-
-            cv::Mat scores(1, classes.size(), CV_32FC1, classes_scores);
-            cv::Point class_id;
-            double maxClassScore;
-
-            minMaxLoc(scores, 0, &maxClassScore, 0, &class_id);
-
-            if (maxClassScore > modelScoreThreshold)
-            {
-                confidences.push_back(maxClassScore);
-                class_ids.push_back(class_id.x);
-
-                float x = data[0];
-                float y = data[1];
-                float w = data[2];
-                float h = data[3];
-
-                int left = int((x - 0.5 * w - pad_x) / scale);
-                int top = int((y - 0.5 * h - pad_y) / scale);
-
-                int width = int(w / scale);
-                int height = int(h / scale);
-
-                boxes.push_back(cv::Rect(left, top, width, height));
-            }
-        }
-        else // yolov5
-        {
-            float confidence = data[4];
-
-            if (confidence >= modelConfidenceThreshold)
-            {
-                float *classes_scores = data+5;
-
-                cv::Mat scores(1, classes.size(), CV_32FC1, classes_scores);
-                cv::Point class_id;
-                double max_class_score;
-
-                minMaxLoc(scores, 0, &max_class_score, 0, &class_id);
-
-                if (max_class_score > modelScoreThreshold)
-                {
-                    confidences.push_back(confidence);
-                    class_ids.push_back(class_id.x);
-
-                    float x = data[0];
-                    float y = data[1];
-                    float w = data[2];
-                    float h = data[3];
-
-                    int left = int((x - 0.5 * w - pad_x) / scale);
-                    int top = int((y - 0.5 * h - pad_y) / scale);
-
-                    int width = int(w / scale);
-                    int height = int(h / scale);
-
-                    boxes.push_back(cv::Rect(left, top, width, height));
-                }
-            }
-        }
-
-        data += dimensions;
-    }
-
-    std::vector<int> nms_result;
-    cv::dnn::NMSBoxes(boxes, confidences, modelScoreThreshold, modelNMSThreshold, nms_result);
-
-    std::vector<Detection> detections{};
-    for (unsigned long i = 0; i < nms_result.size(); ++i)
-    {
-        int idx = nms_result[i];
-
-        Detection result;
-        result.class_id = class_ids[idx];
-        result.confidence = confidences[idx];
-
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<int> dis(100, 255);
-        result.color = cv::Scalar(dis(gen),
-                                  dis(gen),
-                                  dis(gen));
-
-        result.className = classes[result.class_id];
-        result.box = boxes[idx];
-
-        detections.push_back(result);
-    }
-
-    return detections;
-}
-
-
-int main(int argc, char **argv)
-{
-    std::string projectBasePath = "/home/dlyakhov/Projects/ultralytics"; // Set your ultralytics base path
-
-    bool runOnGPU = false;
-
-    //
-    // Pass in either:
-    //
-    // "yolov8s.onnx" or "yolov5s.onnx"
-    //
-    // To run Inference with yolov8/yolov5 (ONNX)
-    //
-
-    executorch::runtime::runtime_init();
-    Module yolo_module(projectBasePath + "/yolov8s.pte");
-
-    auto error = yolo_module.load();
-    // TODO: enable
-    //ET_CHECK_MSG(
-    //    error == Error::ok ,
-    //    "Failed to parse model file %s, error: 0x%",
-    //    projectBasePath.c_str(), error);
-
-    error = yolo_module.load_forward();
-    //ET_CHECK_MSG(
-    //    error == Error::ok ,
-    //    "Failed to get method_meta for forward, error: 0x%", error);
-
-    std::vector<std::string> imageNames;
-    imageNames.push_back(projectBasePath + "/ultralytics/assets/bus.jpg");
-    imageNames.push_back(projectBasePath + "/ultralytics/assets/zidane.jpg");
-
-    for (int i = 0; i < imageNames.size(); ++i)
-    {
-        cv::Mat frame = cv::imread(imageNames[i]);
-
-        // Inference starts here...
-        std::vector<Detection> output = infer_yolo_once(yolo_module, frame);
-
-        int detections = output.size();
-        std::cout << "Number of detections:" << detections << std::endl;
-
-        for (int i = 0; i < detections; ++i)
-        {
-            Detection detection = output[i];
-
-            cv::Rect box = detection.box;
-            cv::Scalar color = detection.color;
-
-            // Detection box
-            cv::rectangle(frame, box, color, 2);
-
-            // Detection box text
-            std::string classString = detection.className + ' ' + std::to_string(detection.confidence).substr(0, 4);
-            cv::Size textSize = cv::getTextSize(classString, cv::FONT_HERSHEY_DUPLEX, 1, 2, 0);
-            cv::Rect textBox(box.x, box.y - 40, textSize.width + 10, textSize.height + 20);
-
-            cv::rectangle(frame, textBox, color, cv::FILLED);
-            cv::putText(frame, classString, cv::Point(box.x + 5, box.y - 10), cv::FONT_HERSHEY_DUPLEX, 1, cv::Scalar(0, 0, 0), 2, 0);
-        }
-        // Inference ends here...
-
-        // This is only for preview purposes
-        float scale = 0.8;
-        cv::resize(frame, frame, cv::Size(frame.cols*scale, frame.rows*scale));
-        //std::raise(SIGINT);
-        cv::imwrite("out_4125_" + std::to_string(i) +".jpg", frame);
-        //cv::imshow("Inference", frame);
-        //cv::waitKey(-1);
-    }
+  cv::rectangle(frame, textBox, color, cv::FILLED);
+  cv::putText(
+      frame,
+      classString,
+      cv::Point(box.x + 5, box.y - 10),
+      cv::FONT_HERSHEY_DUPLEX,
+      1,
+      cv::Scalar(0, 0, 0),
+      2,
+      0);
 }
